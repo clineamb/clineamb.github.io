7ad2a7afe79c83a3adf4140f82e00947ca8227c8
@clineamb <github@carolineamaba.com>
2016-11-07 17:30:20 -0500

    add files

--- gulpfile.js
+++ gulpfile.js
@@ -5,33 +5,18 @@ var gulp            = require('gulp')
 ,   autoprefix      = require('gulp-autoprefixer')
 ,   rename          = require('gulp-rename')
 ,   uglify          = require('gulp-uglify')
-,   livereload      = require('gulp-livereload')
 ,   quickBundle     = require('./tasks/quick-bundle.js')
-,   through2        = require('through2')
 //  SETTINGS
 ,   less_src        = "./less/*.less"
 ,   less_paths      = "./less/**/*.less"
-,   less_dest       = "./public/css"
+,   less_dest       = "./css"
 ,   js_src          = "./src/*.json"
-,   js_dest         = "./public/js"
+,   js_dest         = "./js"
 ;
 
 gulp.task('default', ['less', 'qb']);
 
-gulp.task('less', ['less.dev', 'less.min', 'qb']);
-
-gulp.task('less.dev', function() {
-    return gulp.src(less_src)
-        .pipe(less({
-            'paths': less_paths
-        }))
-        .pipe(autoprefix({
-            'cascade': true,
-            'remove': true
-        }))
-        .pipe(gulp.dest(less_dest))
-    ;
-});
+gulp.task('less', ['less.min']);
 
 gulp.task('less.min', function () {
     return gulp.src(less_src)
@@ -48,15 +33,7 @@ gulp.task('less.min', function () {
     ;
 });
 
-gulp.task('qb', ['js.qb', 'js.qb.min']);
-
-gulp.task('js.qb', function() {
-    return gulp.src(js_src)
-        .pipe(quickBundle())
-        .pipe(rename({ 'extname': '.js' }))
-        .pipe(gulp.dest(js_dest))
-    ;
-});
+gulp.task('qb', ['js.qb.min']);
 
 gulp.task('js.qb.min', function() {
     return gulp.src(js_src)
@@ -65,13 +42,4 @@ gulp.task('js.qb.min', function() {
             .pipe(rename({'extname': '.min.js'}))
         .pipe(gulp.dest(js_dest))
     ;
-});
-
-gulp.task('watch', ['default'], function() {
-    livereload.listen();
-
-    gulp.watch('./less/**/*.less', ['less'])
-        .on('change', livereload.reload);
-    gulp.watch('./src/**/*.js', ['qb'])
-        .on('change', livereload.reload);
 });
